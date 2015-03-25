f86c5cfcaaeee601d5b015737920f752c50379dc
Luke Clewlow <luke.clewlow@gmail.com>
2015-03-25 18:32:31 +0000

    added controller to handle adding new todos

---
+++ app/controllers/todos.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+	actions: {
+		createTodo: function(newTitle) {
+			//Create the new Todo model
+			var todo = this.store.createRecord('todo', {
+				title: newTitle,
+				isCompleted: false
+			});
+
+			// Clear the "New Todo" text field
+			this.set('newTitle', '');
+
+			todo.save();
+		}
+	}
+});
