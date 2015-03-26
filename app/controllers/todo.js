b7b187de53c10ced2e7b134d099b8c8cb8bad495
Luke Clewlow <luke.clewlow@gmail.com>
2015-03-26 09:35:19 +0000

    added edit and delete functionality for todos

---
+++ app/controllers/todo.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+	actions: {
+		editTodo: function() {
+			this.set('isEditing', true);
+		},
+		acceptChanges: function() {
+			//Remove is ediiting property
+			this.set('isEditing', false);
+
+			// If the todo is empty, delete it
+			// otherwise save it with the new title
+
+			if(Ember.isEmpty(this.get('model.title'))) {
+				this.send('removeTodo');
+			} else {
+				this.get('model').save();
+			}
+		},
+		removeTodo: function() {
+			var todo = this.get('model');
+			todo.deleteRecord();
+			todo.save();
+		}
+	}
+});
